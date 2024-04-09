31dc5f491d45e36c23788da19e969653f097e83f
Usama Bhakrani <usamabhakrani@gmail.com>
2024-04-10 03:55:28 +0500

    first server action

--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,6 +1,27 @@
+import { db } from "@/db";
+import { redirect } from "next/navigation";
+
 const SnippetCreatePage = () => {
+  const createSnippet = async (formData: FormData) => {
+    // Server Action
+    "use server";
+    // Validate Input
+    const title = formData.get("title") as string;
+    const code = formData.get("code") as string;
+    // Create new Record in DB
+    const snippet = await db.snippet.create({
+      data: {
+        title,
+        code,
+      },
+    });
+    // Log Snippet
+    console.log(snippet);
+    // Navigate to Snippet List
+    redirect("/");
+  };
   return (
-    <form>
+    <form action={createSnippet}>
       <h3 className="font-bold m-3">Create a Snippet</h3>
       <div className="flex flex-col gap-4">
         <div className="flex gap-4">
@@ -8,7 +29,6 @@ const SnippetCreatePage = () => {
             Title
           </label>
           <input
-            type="text"
             name="title"
             className="border rounded p-2 w-full"
             id="title"
--- src/db/index.ts
+++ src/db/index.ts
@@ -2,9 +2,3 @@ import { PrismaClient } from "@prisma/client";
 
 export const db = new PrismaClient();
 
-db.snippet.create({
-  data: {
-    title: "Hello",
-    code: "Hello",
-  },
-});
