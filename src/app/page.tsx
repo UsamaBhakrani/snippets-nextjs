57465d797df26a4a92a040b88d0f9c8556f17f2b
Usama Bhakrani <usamabhakrani@gmail.com>
2024-04-11 02:36:08 +0500

    show snippets

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
-export default function Home() {
-  return <div>Home Page</div>;
+import { db } from "@/db";
+
+export default async function Home() {
+  const snippets = await db.snippet.findMany();
+  return snippets.map(({ title, id, code }) => {
+    return (
+      <div key={id}>
+        {title}-{code}
+      </div>
+    );
+  });
 }
