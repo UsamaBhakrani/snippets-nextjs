5aff339d7f53ddbd1b12301b9d340500e3f199f7
Usama Bhakrani <usamabhakrani@gmail.com>
2024-04-12 15:52:18 +0500

    SnippetEditForm

--- src/app/snippets/[id]/edit/page.tsx
+++ src/app/snippets/[id]/edit/page.tsx
@@ -1,13 +1,27 @@
+import SnippetEditForm from "@/components/SnippetEditForm";
+import { db } from "@/db";
+import { notFound } from "next/navigation";
+
 interface SnippetEditPageProps {
   params: {
     id: string;
   };
 }
 
-const SnippetEditPage = ({ params }: SnippetEditPageProps) => {
+const SnippetEditPage = async ({ params }: SnippetEditPageProps) => {
   const id = parseInt(params.id);
+  const snippet = await db.snippet.findFirst({
+    where: { id },
+  });
+  if (!snippet) {
+    notFound();
+  }
 
-  return <div>SnippetEditPage {id}</div>;
+  return (
+    <div>
+      <SnippetEditForm snippet={snippet} />
+    </div>
+  );
 };
 
 export default SnippetEditPage;
