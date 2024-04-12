0bffc91b9956b524c6cdb45212443fbe01fee603
Usama Bhakrani <usamabhakrani@gmail.com>
2024-04-12 15:29:36 +0500

    Snippets page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,31 @@
 import { db } from "@/db";
+import Link from "next/link";
 
 export default async function Home() {
   const snippets = await db.snippet.findMany();
-  return snippets.map(({ title, id, code }) => {
+  const renderedSnippets = snippets.map(({ title, id, code }) => {
     return (
-      <div key={id}>
-        {title}-{code}
-      </div>
+      <Link
+        href={`/snippets/${id}`}
+        key={id}
+        className="flex justify-between items-center p-2 border rounded"
+      >
+        <div>
+          {title}-{code}
+        </div>
+        <div>View</div>
+      </Link>
     );
   });
+  return (
+    <div>
+      <div className="flex m-2 justify-between items-center">
+        <h1 className="text-xl font-bold">Snippets</h1>
+        <Link href={`/snippets/new`} className="border p-2 rounded">
+          New
+        </Link>
+      </div>
+      <div className="flex flex-col gap-2">{renderedSnippets}</div>
+    </div>
+  );
 }
