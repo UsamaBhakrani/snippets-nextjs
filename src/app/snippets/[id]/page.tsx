0f457186e8ae61916f8951f2081fa39f02fe12ed
Mohammed Usama <usamabhakrani@gmail.com>
2024-04-13 12:28:25 +0500

    Delete Snippet with bind method for extra arguments

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -1,7 +1,7 @@
 import { db } from "@/db";
 import Link from "next/link";
 import { notFound } from "next/navigation";
-
+import * as actions from "@/actions";
 interface SnippetShowProps {
   params: {
     id: string;
@@ -15,13 +15,22 @@ const SnippetShowPage = async ({ params }: SnippetShowProps) => {
   if (!snippet) {
     return notFound();
   }
+  const deleteSnippetAction = actions.deleteSnippet.bind(null, snippet.id);
+
   return (
     <div>
       <div className="flex m-4 justify-between items-center">
         <h1 className="text-xl font-bold">{snippet.title}</h1>
         <div className="flex gap-4">
-          <Link href={`/snippets/${snippet.id}/edit`} className="p-2 border rounded">Edit</Link>
-          <Link href={`/snippets/${snippet.id}/edit`} className="p-2 border rounded">Delete</Link>
+          <Link
+            href={`/snippets/${snippet.id}/edit`}
+            className="p-2 border rounded"
+          >
+            Edit
+          </Link>
+          <form action={deleteSnippetAction}>
+            <button className="p-2 border rounded">Delete</button>
+          </form>
         </div>
       </div>
       <pre className="p-3 border rounded bg-gray-200 border-gray-200">
--- src/components/SnippetEditForm.tsx
+++ src/components/SnippetEditForm.tsx
@@ -3,6 +3,8 @@
 import { Editor } from "@monaco-editor/react";
 import type { Snippet } from "@prisma/client";
 import { useState } from "react";
+import * as actions from "@/actions";
+
 interface SnippetEditFormProps {
   snippet: Snippet;
 }
@@ -12,6 +14,8 @@ const SnippetEditForm = ({ snippet }: SnippetEditFormProps) => {
   const handleEditorChange = (value: string = "") => {
     setCode(value);
   };
+
+  const editSnippetAction = actions.editSnippet.bind(null, snippet.id, code);
 
   return (
     <div>
@@ -23,6 +27,9 @@ const SnippetEditForm = ({ snippet }: SnippetEditFormProps) => {
         onChange={handleEditorChange}
         options={{ minimap: { enabled: false } }}
       />
+      <form action={editSnippetAction}>
+        <button className="p-2 border rounded">Save</button>
+      </form>
     </div>
   );
 };
